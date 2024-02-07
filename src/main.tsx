b87ef3a58719df7c761d317a6ff066a14eee5806
Mathias Lindholm <mathias.lindholm@gmail.com>
2024-02-07 22:35:01 +0200

    Use modern normalize

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import '/node_modules/modern-normalize/modern-normalize.css';
 import './reset.css';
 import './index.css';
 
